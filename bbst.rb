a2c1c0c7a9215fe6cdf133bc0b4339b44b8fbdf5
AlexTBHoang <alexanderhoangtb@gmail.com>
2023-01-08 22:33:17 +0100

    .

--- bbst.rb
+++ bbst.rb
@@ -1,5 +1,23 @@
 class Node
+
+    def initialize(value, left_child=nil, right_child=nil)
+        @value = value
+        @left_child = left_child
+        @right_child = right_child
+    end
+
 end
 
 class Tree
+
+    def initialize(arr)
+        @root = build_tree(arr)
+    end
+
+    def build_tree(arr)
+        #need to sort it
+        #need to remove duplicates
+        
+    end
+
 end
