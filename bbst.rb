b34054e1bb4e9703405128652971750dc40ebd93
AlexTBHoang <alexanderhoangtb@gmail.com>
2023-06-12 00:36:32 +0200

    working on it

--- bbst.rb
+++ bbst.rb
@@ -1,30 +1,59 @@
 require 'set'
 
 class Node
+    attr_accessor :data, :left, :right
 
-    def initialize(value, left_child=nil, right_child=nil)
-        @value = value
-        @left_child = left_child
-        @right_child = right_child
+    def initialize(value, left=nil, right=nil)
+        @data = value
+        @left = nil
+        @right = nil
     end
 
 end
 
 class Tree
+    attr_accessor :root
 
     def initialize(arr)
-        @root = build_tree(arr)
+        self.root = build_tree(cleanArray(arr))
     end
 
     def build_tree(arr)
-        new_arr = arr.sort
-        new_set = new_arr.to_set
-        
-        #need to sort it, sort
-        #need to remove duplicates, save all the elements in a set, convert the set to an array later
-        
 
-        return @root
+        #end of recursion
+        unless arr.length > 1
+            return arr    
+        end
+
+        root = Node.new((arr.length+1)/2)
+        
+        #recursive function, split the array in half and send it to build_tree function
+        root.left = build_tree(arr.slice(0, (arr.length+1)/2))
+        root.right = build_tree(arr.slice((arr.length+1)/2, arr.length+1))
+        
+        
+        #creating nodes and assingning left/right nodes
+        puts root.data
+        pretty_print
     end
 
+    def cleanArray(arr)
+        new_arr = arr.sort
+        new_set = new_arr.to_set
+        arr = new_set.to_a
+    end
+
+    def pretty_print(node = @root, prefix = '', is_left = true)
+        pretty_print(node.right, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right
+        puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.data}"
+        pretty_print(node.left, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left
+    end
 end
+
+
+array1 = [1, 2, 3, 4 ,5]
+newTree = Tree.new(array1)
+
+
+
+# 1, 2, 3, 4, 5, 8, 9, 10, 11, 12
