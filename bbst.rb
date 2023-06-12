db2174f1a2d5a5e5fe637db945b8cdefae9ec9a3
AlexTBHoang <alexanderhoangtb@gmail.com>
2023-06-13 01:02:41 +0200

    fixed it abit

--- bbst.rb
+++ bbst.rb
@@ -1,40 +1,36 @@
 require 'set'
 
+
 class Node
     attr_accessor :data, :left, :right
 
-    def initialize(value, left=nil, right=nil)
-        @data = value
+    def initialize(data)
+        @data = data
         @left = nil
         @right = nil
     end
-
 end
 
 class Tree
     attr_accessor :root
 
     def initialize(arr)
-        self.root = build_tree(cleanArray(arr))
+        @root = build_tree(cleanArray(arr))
+        
     end
 
     def build_tree(arr)
 
-        #end of recursion
-        unless arr.length > 1
-            return arr    
-        end
+        return nil if arr.empty?        
 
-        root = Node.new((arr.length+1)/2)
+        half = (arr.size-1) / 2
+        root = Node.new(arr[half])
         
-        #recursive function, split the array in half and send it to build_tree function
-        root.left = build_tree(arr.slice(0, (arr.length+1)/2))
-        root.right = build_tree(arr.slice((arr.length+1)/2, arr.length+1))
+        root.left = build_tree(arr[0...half])
+        root.right = build_tree(arr[(half +1)..-1])       
         
-        
-        #creating nodes and assingning left/right nodes
-        puts root.data
-        pretty_print
+
+        root
     end
 
     def cleanArray(arr)
@@ -48,12 +44,15 @@ class Tree
         puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.data}"
         pretty_print(node.left, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left
     end
+
 end
 
 
-array1 = [1, 2, 3, 4 ,5]
-newTree = Tree.new(array1)
 
+
+array1 = [1, 2, 3, 4 ,5, 6, 7, 8]
+newTree = Tree.new(array1)
+newTree.pretty_print
 
 
 # 1, 2, 3, 4, 5, 8, 9, 10, 11, 12
